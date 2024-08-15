9c3398c735500e7644c69d05c22e32ab87597f26
Hydr0x1de <chelovek228hh8@gmail.com>
2024-08-16 00:33:12 +0500

    initial

---
+++ README.md
@@ -0,0 +1,14 @@
+setup
+---
+optionally (recommended) create virtual environment `python3 -m venv venv`
+and activate it `source venv/bin/activate`
+
+install from the requirements.txt
+`pip install -r requirements.txt`
+
+create "config.txt" file
+add the discord bot token in the first line 
+add the port which proxy uses in the second line
+
+run the bot 
+`python3 main.py`
---
+++ main.py
@@ -0,0 +1,59 @@
+from discord.ext import commands
+from discord import Intents
+from os import system
+
+
+#setup
+intents = Intents.default()
+intents.message_content = True
+intents.guilds = True
+intents.guild_messages = True
+bot = commands.Bot(command_prefix='psc!', intents=intents)
+with open('config.txt', 'r') as f: 
+    read = f.readlines()
+    read = [line.strip() for line in read]
+    TOKEN, PORT, *other = read # *other is just protection
+
+
+
+@bot.event
+async def on_ready():
+    print(f'Logged in as {bot.user}')
+
+
+@bot.event
+async def on_message(message):
+    if message.author.bot:
+        return  
+    print(f'message from {message.author} : {message.content}')
+    await bot.process_commands(message)
+
+
+@bot.command()
+async def ping(ctx):
+    await ctx.send('Pong!')
+
+
+@bot.command()
+async def conn(ctx):
+    #get and write to file amount of estabilished connections
+    system("netstat -anp | grep :" + PORT + " | grep ESTABLISHED | awk '{print $5}' | cut -d: -f1 | sort | uniq | wc -l > conn-amount.dat")
+    #send result
+    with open('conn-amount.dat', 'r') as f:
+        result = f.read().strip()
+    await ctx.send(f'{result} devices connected')
+
+
+@bot.command()
+async def connlst(ctx):
+    #get and write to file list of estabilished connections (their IP's actually)
+    system("netstat -anp | grep :" + PORT + " | grep ESTABLISHED | awk '{print $5}' | cut -d: -f1 | sort | uniq > conn-lst.dat")
+    #send result
+    with open('conn-lst.dat', 'r') as f:
+        result = f.read().strip()
+    await ctx.send(f'List of all connected devices:\n{result}')
+
+
+#run
+if __name__ == '__main__':
+    bot.run(TOKEN)
