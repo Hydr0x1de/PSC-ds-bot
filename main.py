496503f68ee3862672d31fb6231967c4abbb9001
Hydr0x1de <chelovek228hh8@gmail.com>
2024-08-17 02:19:08 +0500

    update

--- main.py
+++ main.py
@@ -13,7 +13,9 @@ intents = Intents.default()
 intents.message_content = True
 intents.guilds = True
 intents.guild_messages = True
+
 bot = commands.Bot(command_prefix='psc!', intents=intents)
+
 with open('config.txt', 'r') as f: 
     read = f.readlines()
     read = [line.strip() for line in read]
