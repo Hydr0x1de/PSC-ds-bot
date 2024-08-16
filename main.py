97274a8e9c9bee4c3df2ce3c5a4fa734c15498cd
Hydr0x1de <chelovek228hh8@gmail.com>
2024-08-17 01:15:58 +0500

    update

--- main.py
+++ main.py
@@ -101,8 +101,8 @@ async def serverinfo(ctx):
     disk.total, disk.used, disk.free
     memory = virtual_memory()
     memory.total, memory.used
-    result =  f'**Disk**    total:{get_size(disk.total)} used:{get_size(disk.used)} free:{get_size(disk.free)}\n' \
-            + f'**RAM**     {get_size(memory.used)}/{get_size(memory.total)}\n' \
+    result =  f'**Disk**    total:{get_size(disk.total)}   used:{get_size(disk.used)}   free:{get_size(disk.free)}\n' \
+            + f'**RAM**     {get_size(memory.used)} / {get_size(memory.total)}\n' \
             + f'**CPU**     freq:{cpu_freq().current}MHz / {cpu_freq().max}MHz  perc:{cpu_percent()}%'
     await ctx.send(result)
 
