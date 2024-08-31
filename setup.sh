3d13500d9badfeb44741041f44522ec3de9c57b6
Hydr0x1de <chelovek228hh8@gmail.com>
2024-08-31 11:29:21 +0500

    setup.sh fix

--- setup.sh
+++ setup.sh
@@ -3,5 +3,5 @@ source venv/bin/activate
 pip install -r requirements.txt
 touch restart_ctx.json
 touch config.toml
-echo 'TOKEN = ""' >> config.toml
+echo 'TOKEN = ""' > config.toml
 echo 'PORT = ""' >> config.toml
