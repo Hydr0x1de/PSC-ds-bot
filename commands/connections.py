6996c107f67ca726084f268f936dc5a4b7c6f454
Hydr0x1de <chelovek228hh8@gmail.com>
2024-09-06 01:10:47 +0500

    bugfix

--- commands/connections.py
+++ commands/connections.py
@@ -1,4 +1,5 @@
 from discord.ext import commands
+import toml
 
 from .tools import execute
 
@@ -6,7 +7,7 @@ from .tools import execute
 @commands.command()
 async def conn(ctx):
     """send amount of connected devices"""
-    global PORT
+    PORT = toml.load('../config.toml')['PORT']
     result = execute(
         "netstat -anp | grep :" + PORT + " | grep ESTABLISHED | awk '{print $5}' | cut -d: -f1 | sort | uniq | wc -l").strip()
     await ctx.send(f'{result} devices connected')
@@ -15,7 +16,7 @@ async def conn(ctx):
 @commands.command()
 async def connlst(ctx):
     """send list of IPs of connected devices"""
-    global PORT
+    PORT = toml.load('../config.toml')['PORT']
     #get and write to file list of estabilished connections (their IP's actually)
     result = execute(
         "netstat -anp | grep :" + PORT + " | grep ESTABLISHED | awk '{print $5}' | cut -d: -f1 | sort | uniq")
